436ab64359715cf106b02c456cd16d28a9419cbc
Arjen <tinyq0rsq@gmail.com>
2017-01-19 12:23:43 +0100

    Corrected English prefixes in main.java

---
+++ ParkeerSimulator/src/Parkeersimulator/Main.java
@@ -0,0 +1,13 @@
+// @author Prior IT
+// @version 0.1 Early Alpha
+package Parkeersimulator;
+
+public class Main {
+		// Main method to invoke a simulator view and perform all the core operations of the application.
+	public static void main(String[] args) {
+		// 
+		Simulator simulator = new Simulator();
+		simulator.run();
+	}
+
+}
