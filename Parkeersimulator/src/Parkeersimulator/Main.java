a452d6621037e711967c54e8724035c46220e6c6
Arjen <tinyq0rsq@gmail.com>
2017-01-19 14:29:16 +0100

    .git ignore file removed

---
+++ Parkeersimulator/src/Parkeersimulator/Main.java
@@ -0,0 +1,13 @@
+//@author Prior IT
+//@version 0.1 Early Alpha
+package Parkeersimulator;
+
+public class Main {
+	//Method for performing the program's core operations
+	public static void main(String[] args) {
+		Simulator simulator = new Simulator();
+		simulator.run();
+	}
+
+}
+// De saffier is een kostbare edelsteen. Het is transparant korund. Blauwe stenen van goede kwaliteit worden over het algemeen geclassificieerd als een 
